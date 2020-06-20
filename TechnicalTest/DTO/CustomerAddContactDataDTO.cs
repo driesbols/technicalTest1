6e8013d542d8fafdc505f088cba9b2380dd5f987
Dries Bols <Dries.bols@certia.be>
2020-06-20 19:57:58 +0200

    Add DTO classes

---
+++ TechnicalTest/DTO/CustomerAddContactDataDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechnicalTest.Domain;
+
+namespace TechnicalTest.Data.DTO
+{
+    public class AddContactDataDTO
+    {
+        public string CustomerId { get; set; }
+        private ContactData _contactData;
+        public ContactData ContactData {
+            get => _contactData ??= new ContactData(); 
+            set => _contactData = value ?? new ContactData(); 
+        }
+    }
+}
---
+++ TechnicalTest/DTO/UpdateInvoiceStatusDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechnicalTest.Data.DTO
+{
+    public class UpdateInvoiceStatusDTO
+    {
+        public string InvoiceId { get; set; }
+        public string Status { get; set; }
+    }
+}
