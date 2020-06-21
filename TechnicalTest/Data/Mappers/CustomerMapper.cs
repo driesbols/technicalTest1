driesbols/technicalTest1

commit b85939a0f058c914e9bd138f823d68a577b31288

remove unnecessary code

TechnicalTest/Data/Mappers/CustomerMapper.cs
@@ -12,12 +12,9 @@ namespace TechnicalTest.Data
 {
     public class CustomerMapper : BaseMapper
     {
-        private readonly string _partitionKey;
-        public CustomerMapper() : this("Customers", "id"){ }
-
-        private CustomerMapper(string containerId, string partitionKey) : base(containerId, "/" + partitionKey) 
+        private CustomerMapper() : base("Customers", "/id") 
         {
-            _partitionKey = partitionKey;
+
         }
 
         public async Task CreateCustomer(Customer customer)

TechnicalTest/Data/Mappers/InvoiceMapper.cs
@@ -11,12 +11,9 @@ namespace TechnicalTest.Data
 {
     public class InvoiceMapper : BaseMapper
     {
-        private readonly string _partitionKey;
-        public InvoiceMapper() : this("Invoices", "id") { }
-
-        private InvoiceMapper(string containerId, string partitionKey) : base(containerId, "/" + partitionKey)
+        private InvoiceMapper() : base("Invoices", "/id")
         {
-            _partitionKey = partitionKey;
+        
         }
 
         public async Task CreateInvoice(Invoice invoice)

commit 5e15f440898102e3707e187e0b5be2a349f297ae

fix errors introduced in commit b85939a - mapper classes inaccessible because of protection level

TechnicalTest/Data/Mappers/CustomerMapper.cs
@@ -12,7 +12,7 @@ namespace TechnicalTest.Data
 {
     public class CustomerMapper : BaseMapper
     {
-        private CustomerMapper() : base("Customers", "/id") 
+        public CustomerMapper() : base("Customers", "/id") 
         {
 
         }

TechnicalTest/Data/Mappers/InvoiceMapper.cs
@@ -11,7 +11,7 @@ namespace TechnicalTest.Data
 {
     public class InvoiceMapper : BaseMapper
     {
-        private InvoiceMapper() : base("Invoices", "/id")
+        public InvoiceMapper() : base("Invoices", "/id")
         {
         
         }